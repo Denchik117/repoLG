b123e84f59e52b833a3f9f3192403352e162ff2e
Denys Pshenychnykov <326640@stud.umk.pl>
2025-10-29 14:37:04 +0100

    Edit fun in #1 and edit fun in #2

--- plik1.c
+++ plik1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(){
 	printf("Hello World! I`m file number 1. I`ve been changed by my owner.");
+	int i;
+	for(i = 0; i < 5; i++){
+		printf("I`m BOB\n");
+	}
 	return 0;
 }
--- plik2.c
+++ plik2.c
@@ -3,7 +3,7 @@ int main(){
 	printf("Hello World! I`m file number 2");
 	int i;
 	for(i = 0; i < 5; i++){
-		print(%d\n,i);
+		print("BOB\n");
 	}
 	return 0;
 }
