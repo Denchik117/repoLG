dc54eec53ad2d62a73fe6c3cf07acee77a4a928d
Denys Pshenychnykov <326640@stud.umk.pl>
2025-10-29 14:29:27 +0100

    Add function in file #1.

--- plik1.c
+++ plik1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(){
 	printf("Hello World! I`m file number 1. I`ve been changed by my owner.");
+
+	int i;
+	for(i = 0; i < 5; i++){
+		printf("I`m the %d message\n", i);
+	}
 	return 0;
 }
